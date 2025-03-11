d75718efa21be264f8c0a6b26052a0912bd3e62b
unknown <elgdiab@gmail.com>
2025-03-11 14:51:49 +0200

    Add the header and the nav bar

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Nav bar animatins
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add('navbar-sticky');
+  } else {
+    navbar.classList.remove('navbar-sticky');
+  }
+});
