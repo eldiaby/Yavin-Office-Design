7ae2d80d95d40da55c5ddcd7093d360a7022035e
unknown <elgdiab@gmail.com>
2025-03-12 16:28:27 +0200

    Add the stats section

--- js/script.js
+++ js/script.js
@@ -8,3 +8,31 @@ window.addEventListener('scroll', () => {
     navbar.classList.remove('navbar-sticky');
   }
 });
+
+// Increment countrs in stats section animation
+function incrementStats() {
+  const counters = document.querySelectorAll('.counter');
+
+  counters.forEach((counter) => {
+    counter.innerText = 0;
+
+    const updateCounter = () => {
+      const target = +counter.getAttribute('data-counter-target');
+      const c = +counter.innerText;
+
+      const increment = target / 200;
+
+      if (c < target) {
+        counter.innerText = Math.ceil(c + increment);
+        setTimeout(updateCounter, 1);
+      } else {
+        counter.innerText = target;
+      }
+    };
+
+    updateCounter();
+  });
+}
+
+// Event Listeners
+document.addEventListener('DOMContentLoaded', incrementStats);
